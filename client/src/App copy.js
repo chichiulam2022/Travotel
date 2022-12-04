5d5611d5487d12b132ab1ae4615680f7d4d4ba46
nab-man <nab.the.rectifier@gmail.com>
2022-12-04 00:16:14 -0500

    branch update

---
+++ client/src/App copy.js
@@ -0,0 +1,26 @@
+import Homepage from "./pages/Homepage";
+import { Routes, Route } from "react-router-dom";
+
+import RoomSlider from "./components/Slider/HotelSlider";
+import NavBar from "./components/Navbar/NavBar";
+import RoomCards from "./components/RoomCards/RoomCards";
+import FrontPage from "./components/FrontPage/FrontPage";
+import AboutUs from "./components/AboutUs/AboutUs";
+
+
+function App() {
+  return (
+    <Routes>
+
+      <Route index element={<FrontPage />} />
+      <Route path="home" element={<Homepage />} />
+      <Route path="/slider" element={<RoomSlider />} />
+      <Route path="/navbar" element={<NavBar />} />
+      <Route path="/roomCard" element={<RoomCards />} />
+      <Route path="/aboutus" element={<AboutUs />} />
+    </Routes >
+
+  )
+}
+
+export default App;
---
+++ client/src/components/AboutUs/AboutUs.js
@@ -0,0 +1,83 @@
+import React from 'react'
+//import './Footer.css'
+import Card from 'react-bootstrap/Card';
+
+function AboutUs() {
+  return (
+    <div>
+        <p>HELLO</p>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          KEEVE
+        </Card.Text>
+        <Card.Link href="#">GitHub</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          CHI CHIU
+        </Card.Text>
+        <Card.Link href="#">GitHub</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          ALEX
+        </Card.Text>
+        <Card.Link href="#">GitHub</Card.Link>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          ANDY
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          AASHAY
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          NABEEL
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+    </div>
+  );
+}
+
+export default AboutUs;
