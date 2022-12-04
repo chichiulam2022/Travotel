98d5f7ee08ab5f19d1aab5c36636c6f54b52a457
nab-man <nab.the.rectifier@gmail.com>
2022-12-04 13:06:21 -0500

    About Us Online

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,19 @@ import { Routes, Route } from "react-router-dom";
 import RoomSlider from "./components/Slider/HotelSlider";
 import NavBar from "./components/Navbar/NavBar";
 //import RoomCards from "./components/RoomCards/RoomCards";
+import FrontPage from "./components/FrontPage/FrontPage";
 import AboutUs from "./components/AboutUs/AboutUs";
 
 
 function App() {
   return (
     <Routes>
-      <Route index element={<Homepage />} />
+      <Route index element={<FrontPage />} />
+      <Route path="home" element={<Homepage />} />
+      <Route path="/slider" element={<RoomSlider />} />
+      <Route path="/navbar" element={<NavBar />} />
+      {/* <Route path="/roomCard" element={<RoomCards />} /> */}
+      <Route path="/aboutus" element={<AboutUs />} />
     </Routes >
 
   )
