233503c5edd54d8b10ed20b4b09fa7677530b9c2
nab-man <nab.the.rectifier@gmail.com>
2022-12-04 11:04:31 -0500

    Room Cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import Homepage from "./pages/Homepage";
 import { Routes, Route } from "react-router-dom";
 
+import RoomSlider from "./components/Slider/HotelSlider";
+import NavBar from "./components/Navbar/NavBar";
+//import RoomCards from "./components/RoomCards/RoomCards";
+import AboutUs from "./components/AboutUs/AboutUs";
+
 
 function App() {
   return (
