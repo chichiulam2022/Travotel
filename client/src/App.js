a99bd79c8c3c33d6f4c9bbee63bfc2416602e538
KeeveRW11 <keevewhyte+github@gmail.com>
2022-12-04 15:41:00 -0500

    server client link

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,28 @@ import Homepage from "./pages/Homepage";
 import { Routes, Route } from "react-router-dom";
 import RoomSlider from "./components/Slider/RoomSlider";
 import NavBar from "./components/Navbar/NavBar";
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3005/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Routes>
       <Route index element={<FrontPage />} />
       <Route path="home" element={<Homepage />} />
       <Route path="/slider" element={<RoomSlider />} />
       <Route path="/navbar" element={<NavBar />} />
     </Routes >
+    </ApolloProvider>
 
   )
 }
