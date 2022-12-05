791b1aee263db6cde5fd1ef0035e2be0d404dfbf
KeeveRW11 <keevewhyte+github@gmail.com>
2022-12-04 20:23:31 -0500

    up to date with develop

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import Homepage from "./pages/Homepage";
 import { Routes, Route } from "react-router-dom";
-import RoomSlider from "./components/Slider/RoomSlider";
-import NavBar from "./components/Navbar/NavBar";
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Rooms from "./pages/Rooms";
+import Review from "./components/Review/Review";
+import LoginPage from './pages/LoginPage/LoginPage'
+import SignupPage from './pages/SignupPage/SignupPage'
+import './App.css'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3005/graphql',
@@ -12,23 +15,18 @@ const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
 });
-import Rooms from "./pages/Rooms";
-import Review from "./components/Review/Review";
-import LoginPage from './pages/LoginPage/LoginPage'
-import SignupPage from './pages/SignupPage/SignupPage'
-import './App.css'
-
 
 
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Routes>
-      <Route index element={<FrontPage />} />
-      <Route path="home" element={<Homepage />} />
-      <Route path="/slider" element={<RoomSlider />} />
-      <Route path="/navbar" element={<NavBar />} />
-    </Routes >
+<Routes>
+        <Route index element={<Homepage />} />
+        <Route path="/toronto" element={<Rooms />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes >
     </ApolloProvider>
 
   )
